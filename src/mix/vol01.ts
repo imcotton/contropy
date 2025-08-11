d6ccb053f00736938c65ef74acd4ef17a8a12380
Cotton Hou <himcotton@gmail.com>
2025-08-12 02:34:52 +0800

    mix/vol01

---
+++ src/mix/vol01.ts
@@ -0,0 +1,47 @@
+import { pi } from '../pi.js';
+
+import {
+
+    reverse, traverse, sort_by_slice, join, truncate_hex, mul,
+    sha512, decode_hex, hmac, buf_to_bigint,
+    btc, drand, jsr, npm, crate,
+
+} from './common.ts';
+
+
+
+
+
+export async function vol01 (pepper: string, n = 1024): Promise<ArrayBuffer> {
+
+    const grind = hmac('SHA-512', pi(n).toString(16));
+
+    const seed = await grind(reverse(pepper)).then(buf_to_bigint);
+
+    const plant = traverse([
+
+        btc({ tip: 909_694 }),
+        drand({ network: 'default',  tip:  5_319_100 }),
+        drand({ network: 'quicknet', tip: 20_733_559 }),
+
+    ]);
+
+    const queue = plant(seed).concat([
+
+        crate(      'tokio', '1.8.4'),
+        npm(   'typescript', '5.9.2'),
+        jsr('@std/encoding', '1.0.7'),
+
+    ]);
+
+    return Promise.all(queue)
+        .then(sort_by_slice(2, 4))
+        .then(join)
+        .then(mul(seed))
+        .then(truncate_hex)
+        .then(decode_hex)
+        .then(sha512)
+    ;
+
+}
+
